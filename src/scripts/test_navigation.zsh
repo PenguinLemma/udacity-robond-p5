422f5386ceee9b99b87cbf3aa6173da1396b245d
PenguinLemma <thepenguinlemma@gmail.com>
2019-06-03 10:40:00 +0200

    Add scripts to test navigation

---
+++ src/scripts/test_navigation.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+xterm -e " source devel/setup.zsh; roslaunch turtlebot_gazebo turtlebot_world.launch " &
+sleep 5
+xterm -e " source devel/setup.zsh; roslaunch turtlebot_gazebo amcl_demo.launch " &
+sleep 5
+xterm -e " source devel/setup.zsh; roslaunch turtlebot_rviz_launchers view_navigation.launch "
