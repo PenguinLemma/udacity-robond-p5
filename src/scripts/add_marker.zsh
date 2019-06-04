9a881b347a10e413d75a8863c015fdcf013f5e73
PenguinLemma <thepenguinlemma@gmail.com>
2019-06-04 13:30:35 +0000

    Add scripts to see add_markers in action

---
+++ src/scripts/add_marker.zsh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+xterm -e " source devel/setup.zsh; roslaunch turtlebot_gazebo turtlebot_world.launch " &
+sleep 5
+xterm -e " source devel/setup.zsh; roslaunch turtlebot_gazebo amcl_demo.launch " &
+sleep 5
+xterm -e " source devel/setup.zsh; roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 5
+xterm -e " source devel/setup.zsh; rosrun add_markers add_markers_node "
