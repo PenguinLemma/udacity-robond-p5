003805fe5690ff39aa59001486371842fe14933d
PenguinLemma <thepenguinlemma@gmail.com>
2019-06-09 17:15:12 +0000

    Fix slam scripts
    Wrong launch file was used

--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -2,7 +2,7 @@
 
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$ROOT_HSR_REPO_PATH/src/world/home_service_robot_test.world " &
 sleep 5
-xterm -e " roslaunch gmapping slam_gmapping_pr2.launch " &
+xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch " &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
